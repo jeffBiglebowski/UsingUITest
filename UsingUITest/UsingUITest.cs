21a7424862fbedde54f00b1a59963f743a68c008
mroubicek <mroubicek@ROUBICEK-NB>
2018-01-17 14:58:57 +0100

    s

--- UsingUITest/UsingUITest.cs
+++ UsingUITest/UsingUITest.cs
@@ -12,7 +12,7 @@ namespace UsingUITest
 		public MyPage ()
 		{
 			var b = new Button {
-				Text = "Click me",
+				Text = "Clicks me",
 				AutomationId = "MyButton"		// referenced in UITests
 			};
 			b.Clicked += (sender, e) => {
